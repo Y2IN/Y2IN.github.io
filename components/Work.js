fd96dea4bb4cca89af67dff8214918ca4c884d1a
Y2IN <dpdls0228@daum.net>
2022-10-20 17:13:08 +0900

    3

---
+++ components/Work.js
@@ -0,0 +1,25 @@
+export default function Work() {
+  return (
+    <>
+      <div className="py-6">
+        <div>
+          <h1 className="pb-0 text-center">Things I do</h1>
+        </div>
+        <div className="margin:0 grid gap-10 text-center md:grid-cols-2 lg:grid-cols-3">
+          <div>
+            <h2 className="mb-0">a</h2>
+            <p>{/* 내용 */}</p>
+          </div>
+          <div>
+            <h2>b</h2>
+            <p>{/* 내용 */}</p>
+          </div>
+          <div>
+            <h2>c</h2>
+            <p>{/* 내용 */}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import Welcome from '@/components/Welcome'
+import Work from '@/components/Work'
 
 import NewsletterForm from '@/components/NewsletterForm'
 
@@ -23,6 +24,7 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="prose space-y-2 pt-6 pb-6 dark:prose-dark md:space-y-5">
           <Welcome />
+          <Work />
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Recent Posts
           </h1>

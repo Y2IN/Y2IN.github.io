6ad1b505cba9d8a0b3b4801ff6893d3c579770c8
Y2IN <dpdls0228@daum.net>
2022-10-23 20:01:28 +0900

    libft+

--- pages/tags.js
+++ pages/tags.js
@@ -1,44 +1,55 @@
-import Link from '@/components/Link'
-import { PageSEO } from '@/components/SEO'
-import Tag from '@/components/Tag'
+import { TagSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
+import ListLayout from '@/layouts/ListLayout'
+import generateRss from '@/lib/generate-rss'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
 import { getAllTags } from '@/lib/tags'
 import kebabCase from '@/lib/utils/kebabCase'
+import fs from 'fs'
+import path from 'path'
 
-export async function getStaticProps() {
+const root = process.cwd()
+
+export async function getStaticPaths() {
   const tags = await getAllTags('blog')
 
-  return { props: { tags } }
+  return {
+    paths: Object.keys(tags).map((tag) => ({
+      params: {
+        tag,
+      },
+    })),
+    fallback: false,
+  }
 }
 
-export default function Tags({ tags }) {
-  const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
+export async function getStaticProps({ params }) {
+  const allPosts = await getAllFilesFrontMatter('blog')
+  const filteredPosts = allPosts.filter(
+    (post) => post.draft !== true && post.tags.map((t) => kebabCase(t)).includes(params.tag)
+  )
+
+  // rss
+  if (filteredPosts.length > 0) {
+    const rss = generateRss(filteredPosts, `tags/${params.tag}/feed.xml`)
+    const rssPath = path.join(root, 'public', 'tags', params.tag)
+    fs.mkdirSync(rssPath, { recursive: true })
+    fs.writeFileSync(path.join(rssPath, 'feed.xml'), rss)
+  }
+
+  return { props: { posts: filteredPosts, tag: params.tag } }
+}
+
+export default function Tag({ posts, tag }) {
+  // Capitalize first letter and convert space to dash
+  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
   return (
     <>
-      <PageSEO title={`Tags - ${siteMetadata.author}`} description="Things I blog about" />
-      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
-        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
-            Tags
-          </h1>
-        </div>
-        <div className="flex max-w-lg flex-wrap">
-          {Object.keys(tags).length === 0 && 'No tags found.'}
-          {sortedTags.map((t) => {
-            return (
-              <div key={t} className="mt-2 mb-2 mr-5">
-                <Tag text={t} />
-                <Link
-                  href={`/tags/${kebabCase(t)}`}
-                  className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
-                >
-                  {` (${tags[t]})`}
-                </Link>
-              </div>
-            )
-          })}
-        </div>
-      </div>
+      <TagSEO
+        title={`${tag} - ${siteMetadata.author}`}
+        description={`${tag} tags - ${siteMetadata.author}`}
+      />
+      <ListLayout posts={posts} title={title} />
     </>
   )
 }
